05301555868e7262e51a95c8567fa68beb02e00d
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-17 12:34:36 +0530

    feat: Set DATABASE_URL in database/connection.py

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,2 +1,4 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:password@localhost/ecommerce_db"

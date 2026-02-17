5349d013bef98f15e1500245e7c6c8fc2bb981d1
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-17 12:39:48 +0530

    feat: Set AsyncSessionLocal in database/connection.py

--- app/database/connection.py
+++ app/database/connection.py
@@ -4,3 +4,4 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "postgresql+asyncpg://ecommerce_user:password@localhost/ecommerce_db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)

c7a020b06e3b560ed9f17a87c2079b1c20e59e8e
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-17 12:38:11 +0530

    feat: Create async engine in database/connection.py

--- app/database/connection.py
+++ app/database/connection.py
@@ -2,3 +2,5 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql+asyncpg://ecommerce_user:password@localhost/ecommerce_db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)

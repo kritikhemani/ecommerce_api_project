a9c701dd9a6cbc66cdf996ea3fffc6ac71fc9821
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-19 12:36:54 +0530

    feat: Update Product class in models/product.py

--- app/models/product.py
+++ app/models/product.py
@@ -3,3 +3,7 @@ from app.database.connection import Base
 
 class Product(Base):
     __tablename__ = 'products'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    price = Column(Float)

9b6d412cc43ad016f2be68036ae770997f5d2575
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-21 22:44:04 +0530

    feat: Import get_db from app.database.connection to routers/products.py

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.database.connection import get_db
 
 
 router = APIRouter(prefix="/products", tags=["Products"])

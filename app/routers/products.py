ca0a0e0439c6335c6df826fdb8d69884a8cda3c2
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-23 09:42:41 +0530

    feat: Update routers/products.py

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.connection import get_db
-from models.product import Product
+from app.models.product import Product
 from app.schemas.product import CreateProduct, ProductResponse
 from app.cache.redis import get_cache, set_cache
 

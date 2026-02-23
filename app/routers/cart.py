c6da3f5e6fa0322a1a8968eef35b90317d4aa069
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-23 21:06:53 +0530

    feat: Import select from sqlalchemy to routers/cart.py

--- app/routers/cart.py
+++ app/routers/cart.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.connection import get_db
 from app.models.cart import Cart
 from app.schemas.cart import CartCreate, CartResponse
+from sqlalchemy import select
 
 
 router = APIRouter(prefix="/cart", tags=["Cart"])

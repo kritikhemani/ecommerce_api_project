9126594b6ef9fac6d5382ce8596c9615d5443668
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-23 20:29:21 +0530

    feat: Create an async add_to_cart function in routers/cart.py

--- app/routers/cart.py
+++ app/routers/cart.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.connection import get_db
 from app.models.cart import Cart
-from app.schemas.cart import CreateCart, CartResponse
+from app.schemas.cart import CartCreate, CartResponse
 
 
 router = APIRouter(prefix="/cart", tags=["Cart"])
+
+@router.post("/", response_model=CartResponse)
+async def add_to_cart(cart: CartCreate, db: AsyncSession = Depends(get_db)):
+    pass

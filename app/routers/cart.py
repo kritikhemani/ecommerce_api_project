a274f926e8bf2be959723a6cd36aeef7e10c97d8
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-23 20:27:54 +0530

    feat: Import CartCreate and CartResponse from app.schemas.cart to routers/cart.py

--- app/routers/cart.py
+++ app/routers/cart.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.connection import get_db
 from app.models.cart import Cart
+from app.schemas.cart import CreateCart, CartResponse
 
 
 router = APIRouter(prefix="/cart", tags=["Cart"])

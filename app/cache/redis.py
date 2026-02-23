32f42f33e499a98c86e06b1a87a9064cba4d3e3a
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-23 09:20:29 +0530

    feat: Update get_cache function in cache/redis.py

--- app/cache/redis.py
+++ app/cache/redis.py
@@ -4,4 +4,7 @@ import json
 redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 def get_cache(key: str):
-    pass
+    data = redis_client.get(key)
+    if data:
+        return json.loads(data)
+    return None

233f3dc116a1628648c9e031020465847607cc77
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-23 09:41:43 +0530

    feat: Import get_cache and set_cache from app.cache.redis to routers/products.py

--- app/routers/products.py
+++ app/routers/products.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.connection import get_db
 from models.product import Product
 from app.schemas.product import CreateProduct, ProductResponse
+from app.cache.redis import get_cache, set_cache
 
 
 router = APIRouter(prefix="/products", tags=["Products"])

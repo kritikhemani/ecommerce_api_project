9e039508270ff6818c7ab0b8b1bc99e01b92be41
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-23 09:27:02 +0530

    feat: Import Product from models.product to routers/products.py

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.connection import get_db
+from models.product import Product
 
 
 router = APIRouter(prefix="/products", tags=["Products"])

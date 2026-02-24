748c2ddcfddae4cd4a9d2d2149024cdde9c1b410
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-24 10:40:02 +0530

    feat: Import Product from app.models.product to routers/checkout.py

--- app/routers/checkout.py
+++ app/routers/checkout.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.connection import get_db
 from app.models.cart import Cart
+from app.models.product import Product
 
 router = APIRouter(prefix="/checkout", tags=["Checkout"])
 

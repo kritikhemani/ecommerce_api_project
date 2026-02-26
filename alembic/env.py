c09830b28bd2ee1ff0053d412f7315daeb2a12f9
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-26 21:46:00 +0530

    feat: Making some changes to alembic/versions/env.py

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.database.connection import Base
-from app import models
+from app.models import product, cart, order
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.

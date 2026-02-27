b2ec3a9c50920495b2f06904a85c27174fb3f04c
Kriti Khemani <kritikhemani04@gmail.com>
2026-02-27 17:37:55 +0530

    feat: Update OrderResponse class in schemas/order.py

--- app/schemas/order.py
+++ app/schemas/order.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, ConfigDict
 class OrderResponse(BaseModel):
     id: int
     user_id: int
-    total_amount: float
+    total_price: float
     model_config = ConfigDict(from_attributes=True)

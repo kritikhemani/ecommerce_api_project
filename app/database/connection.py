c62621646b9fa3acc17c8eae8ccba7a3b74fc427
Kriti Khemani <kritikhemani04@gmail.com>
2026-05-19 21:11:32 +0530

    Moved DB credentials to env

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,7 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost/ecommerce_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
